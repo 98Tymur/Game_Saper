bc60df7258a1dd0fa89f21f7f1491d021aec3b47
Tymur <timur.sylka@gmail.com>
2022-12-16 17:29:16 +0200

    Add project files.

---
+++ Game_Saper/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Game_Saper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x20ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x10ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x5ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(623, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sizeToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // sizeToolStripMenuItem
+            // 
+            this.sizeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.x20ToolStripMenuItem,
+            this.x10ToolStripMenuItem,
+            this.x5ToolStripMenuItem});
+            this.sizeToolStripMenuItem.Name = "sizeToolStripMenuItem";
+            this.sizeToolStripMenuItem.Size = new System.Drawing.Size(119, 26);
+            this.sizeToolStripMenuItem.Text = "Size";
+            // 
+            // x20ToolStripMenuItem
+            // 
+            this.x20ToolStripMenuItem.Name = "x20ToolStripMenuItem";
+            this.x20ToolStripMenuItem.Size = new System.Drawing.Size(131, 26);
+            this.x20ToolStripMenuItem.Text = "20x20";
+            this.x20ToolStripMenuItem.Click += new System.EventHandler(this.x20ToolStripMenuItem_Click);
+            // 
+            // x10ToolStripMenuItem
+            // 
+            this.x10ToolStripMenuItem.Name = "x10ToolStripMenuItem";
+            this.x10ToolStripMenuItem.Size = new System.Drawing.Size(131, 26);
+            this.x10ToolStripMenuItem.Text = "10x10";
+            this.x10ToolStripMenuItem.Click += new System.EventHandler(this.x10ToolStripMenuItem_Click);
+            // 
+            // x5ToolStripMenuItem
+            // 
+            this.x5ToolStripMenuItem.Name = "x5ToolStripMenuItem";
+            this.x5ToolStripMenuItem.Size = new System.Drawing.Size(131, 26);
+            this.x5ToolStripMenuItem.Text = "5x5";
+            this.x5ToolStripMenuItem.Click += new System.EventHandler(this.x5ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 626);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x20ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x10ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x5ToolStripMenuItem;
+    }
+}
---
+++ Game_Saper/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_Saper
+{
+    public partial class Form1 : Form
+    {
+        int height = 20;
+        int width = 20;
+        int distanceBetweenButtons = 30;
+        int percentBombs = 30;
+        bool isFirstClick = true;
+        int cellsOpened = 0;
+        int bombs = 0;
+
+        ButtonExtended[,] allButtons;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        void FieldClick(object sender, MouseEventArgs e)
+        {
+            ButtonExtended button = (ButtonExtended)sender;
+            // LBC
+            if(e.Button == MouseButtons.Left && button.isClick)
+            {
+                if (button.isBomb)
+                {
+                    if(isFirstClick)
+                    {
+                        button.isBomb = false;
+                        isFirstClick = false;
+                        OpenRegion(button.xX, button.jJ, button);
+                    }
+                    else
+                    {
+                        Explode();
+                    }
+                }
+                else
+                {
+                    EmptyFieldClick(button);
+                }
+
+                isFirstClick = false; 
+            }
+            //RBC
+            if(e.Button == MouseButtons.Right)
+            {
+                button.isClick = !button.isClick;
+
+                if(!button.isClick)
+                {
+                    button.Text = "B";
+                }
+                else
+                {
+                    button.Text = "";
+                }
+            }
+
+            CheckWin();
+        }
+
+        void Explode()
+        {
+            foreach(ButtonExtended button in allButtons)
+            {
+                if(button.isBomb)
+                {
+                    button.Text = "#";
+                }
+            }
+
+            MessageBox.Show("You lost. The end!");
+            //Application.Restart();
+        }
+
+        void EmptyFieldClick(ButtonExtended button)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (allButtons[x, j] == button)
+                    {
+                        OpenRegion(x, j, button);
+                    }
+                }
+            }
+
+            CheckWin();
+        }
+
+        void OpenRegion(int xX, int jJ, ButtonExtended button)
+        {
+            Queue<ButtonExtended> queue = new Queue<ButtonExtended>();
+            queue.Enqueue(button);
+
+            while(queue.Count > 0)
+            { 
+                ButtonExtended currentCell = queue.Dequeue();
+
+                OpenCell(currentCell.xX, currentCell.jJ, currentCell);
+
+                cellsOpened++;
+
+                if (CountBombsAround(currentCell.xX, currentCell.jJ) == 0)
+                {
+                    for (int j = currentCell.jJ - 1; j <= currentCell.jJ + 1; j++)
+                    {
+                        for (int x = currentCell.xX - 1; x <= currentCell.xX + 1; x++)
+                        {
+                            if (x >= 0 && x < width && j < height && j >= 0)
+                            {
+                                if (!allButtons[x, j].wasAdded)
+                                {
+                                    queue.Enqueue(allButtons[x, j]);
+                                    allButtons[x, j].wasAdded = true;
+                                }
+                            } 
+                        }
+                    }
+                }
+            }
+        }
+
+        void OpenCell(int x, int j, ButtonExtended button)
+        {
+            int bombsAround = CountBombsAround(x, j);
+            if (bombsAround == 0)
+            {
+
+            }
+            else
+            {
+                button.Text = "" + bombsAround;
+            }
+
+            button.Enabled = false;
+        }
+
+        int CountBombsAround(int xX, int jJ)
+        {
+            int bombCount = 0;
+
+            for (int x = xX - 1; x <= xX + 1; x++)
+            {
+                for (int j = jJ - 1; j <= jJ + 1; j++)
+                {
+                    if (x >= 0 && x < width && j >= 0 && j < height)
+                    {
+                        if (allButtons[x, j].isBomb)
+                        {
+                            bombCount++;
+                        }
+                    }
+                }
+            }
+
+            return bombCount;
+        }
+
+        void GenerateField()
+        {
+            allButtons = new ButtonExtended[width, height];
+
+            Random random = new Random();
+
+            for (int x = 10; (x - 10) < width * distanceBetweenButtons; x += distanceBetweenButtons)
+            {
+
+                for (int j = 25; (j - 25) < height * distanceBetweenButtons; j += distanceBetweenButtons)
+                {
+                    ButtonExtended button = new ButtonExtended();
+
+                    button.Location = new Point(x, j);
+                    button.Size = new Size(30, 30);
+                    button.isClick = true;
+                    
+                    if (random.Next(0, 100) < percentBombs)
+                    {
+                        button.isBomb = true;
+                    }
+
+                    button.xX = x;
+                    button.jJ = j;
+                    allButtons[(x - 10) / distanceBetweenButtons, (j - 25) / distanceBetweenButtons] = button;
+
+                    Controls.Add(button);
+
+                    button.MouseUp += new MouseEventHandler(FieldClick);
+                }
+            }
+        }
+
+        private void x20ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            width = 20;
+            height = 20;
+            GenerateField();
+        }
+
+        private void x10ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            width = 10;
+            height = 10;
+            GenerateField();
+        }
+
+        private void x5ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            width = 5;
+            height = 5;
+            GenerateField();
+        }
+
+        void CheckWin()
+        {
+            int cells = width * height;
+            int emptyCells = cells - bombs;
+            if(cellsOpened >= emptyCells)
+            {
+                MessageBox.Show("You win!" + emptyCells + " " + cellsOpened);
+            }
+        }
+    }
+
+    class ButtonExtended : Button
+    {
+        public bool isBomb;
+        public bool isClick;
+        public bool wasAdded;
+        public int xX, jJ;
+    }
+}
